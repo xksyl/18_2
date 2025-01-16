aff77ed8bea9d2a73e3d3a17e354769ead4dbb6e
xksyl <egorka2004212@gmail.com>
2025-01-16 14:37:49 +0500

    Add files via upload

--- pythonProjectDjango/UrbanDjango_Project222/UrbanDjango/UrbanDjango/urls.py
+++ pythonProjectDjango/UrbanDjango_Project222/UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from UrbanDjango.task2.views import function, ClassBV
+from task2.views import function, ClassBV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
